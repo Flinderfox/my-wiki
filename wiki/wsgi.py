b6e66d3e5d723a5745d27f9dd79e8a0b62a3b741
Flinderfox <flinderfox@gmail.com>
2021-10-16 09:33:02 +0300

    Added settings_path in wsgi.py

--- wiki/wsgi.py
+++ wiki/wsgi.py
@@ -8,9 +8,31 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+settings_path = '/home/Flinderfox/flinderfox.pythonanywhere.com'
+sys.path.insert(0, settings_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+"""
+# This file contains the WSGI config
+# uration required to serve up your
+# Django app
+import os
+import sys
+
+# Add your project directory to the sys.path
+settings_path = '/home/Flinderfox/flinderfox.pythonanywhere.com'
+sys.path.insert(0, settings_path)
+
+# Set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wiki.settings'
+
+# Set the 'application' variable to the Django wsgi app
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+"""
